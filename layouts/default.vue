52d13792b541c5b766d286d92888e080617b3a0e
LeBonMimo <schwab.guillaume2310@gmail.com>
2025-01-14 10:19:19 +0100

    layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,12 +7,12 @@
         </NuxtLink>
         <div class="menu">
           <button v-if="menuOpen" @click="showMobileMenu">
-            <Icon  icon="ph:list" width="32" color="#FFFFFF"/>
+            <Icon  icon="ph:x" width="32" color="#FFFFFF"/>
           </button>
           <button v-else @click="showMobileMenu">
-            <Icon icon="ph:x" width="32" color="#FFFFFF"/>
+            <Icon icon="ph:list" width="32" color="#FFFFFF"/>
           </button>
-          <ul :class="['menu-items', !menuOpen ? 'menu-open' : ''] ">
+          <ul :class="['menu-items', menuOpen ? 'menu-open' : ''] ">  
             <li><NuxtLink class="menu-link" to="/">Accueil</NuxtLink></li>
             <li><NuxtLink class="menu-link" to="#projects">Mes projets</NuxtLink></li>
             <li><NuxtLink class="menu-link" to="/about">Présentation</NuxtLink></li>
@@ -23,9 +23,23 @@
   </div>
 
   <!-- output page content -->
-  <div>
+  <div class="page-content">
     <slot />
   </div>
+  <footer>
+    <NuxtLink to="/">
+      <img src="@/assets/images/logo.svg" />
+    </NuxtLink>
+    <div class="social-links">
+      <NuxtLink to="#">
+        <Icon class="insta-icon" icon="ph:instagram-logo" width="32" color="#FFFFFF"/>
+      </NuxtLink>
+      <NuxtLink to="#">
+        <Icon icon="ph:linkedin-logo" width="32" color="#FFFFFF"/>
+      </NuxtLink>
+    </div>
+    <p>© 2025 - Tous droits réservés</p>
+  </footer>
 </template>
 
 <script setup lang="ts">
@@ -35,7 +49,6 @@ const menuOpen = ref(false)
 
 const showMobileMenu = () =>  {
   menuOpen.value = !menuOpen.value
-  console.log(menuOpen.value)
 }
 </script>
 

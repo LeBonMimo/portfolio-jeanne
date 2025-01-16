35d927394dd7c76861af1c8e711b2768e5d70dad
LeBonMimo <schwab.guillaume2310@gmail.com>
2025-01-16 11:50:01 +0100

    partie projet

---
+++ components/Project.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="project">
+    <div class="project-img">
+      <img src="@/assets/images/Family_Trip.png" alt="cover 1">
+    </div>
+    <div class="project-description">
+      <h3>Family Trip</h3>
+      <p>Dessin, 2024</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+  @use "@/assets/css/projects.scss";
+</style>
---
+++ components/Projects.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="projects">
+    <h2>Mes projets</h2>
+    <div class="projects-container">
+      <div class="project-grid">
+        <Project />
+        <Project />
+        <Project />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+@use "@/assets/css/projects.scss";
+</style>

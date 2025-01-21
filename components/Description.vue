298dd581cf7b6df6268c6a3e3203221877baa971
LeBonMimo <schwab.guillaume2310@gmail.com>
2025-01-21 12:31:05 +0100

    formulaire de contact

--- components/Description.vue
+++ components/Description.vue
@@ -2,17 +2,38 @@
   <section class="description">
     <div class="description-container">
       <div class="description-text">
-        <h1><span>Hey, moi c’est Jeanne !</span></h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <span class="small-circle">Olalao Jeanne</span> felis in auctor tempor. Mauris vitae
+        <h1>
+          <span>Hey, moi c’est Jeanne !</span>
+          <svg width="45" height="43" viewBox="0 0 45 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M9.9544 21.2011C10.1844 19.3487 11.3706 17.2095 12.0361 15.8889C12.9335 14.1082 13.7957 12.0718 14.767 10.5027C15.8026 8.82976 16.8217 6.9484 17.9114 5.47721C18.4138 4.79892 19.129 3.32239 19.6214 2.90422"
+              stroke="currentColor" stroke-width="5" stroke-linecap="round" />
+            <path
+              d="M21.7385 33.1558C22.9539 31.4207 25.3899 29.9831 26.8188 29.0546C28.7456 27.8025 30.765 26.2698 32.6654 25.2775C34.6914 24.2196 36.8068 22.9404 38.7892 22.1201C39.7032 21.7419 41.2685 20.6859 42.0369 20.5651"
+              stroke="currentColor" stroke-width="5" stroke-linecap="round" />
+            <path
+              d="M3 17.6484C3.14208 16.504 3.28416 15.3597 3.42623 14.2153C3.55464 13.181 4.34703 12.2138 4.48413 11.1096"
+              stroke="currentColor" stroke-width="5" stroke-linecap="round" />
+            <path
+              d="M16.622 25.5457C17.5001 24.2637 18.3782 22.9817 19.2563 21.6997C20.05 20.541 21.2601 19.6429 22.1075 18.4058"
+              stroke="currentColor" stroke-width="5" stroke-linecap="round" />
+            <path
+              d="M25.752 40.1879C26.1691 40.0717 26.6025 40.1971 27.0196 40.1099C27.3925 40.032 27.6788 39.7049 28.0648 39.6801C28.738 39.6369 29.3884 39.2785 30.0248 39.273"
+              stroke="currentColor" stroke-width="5" stroke-linecap="round" />
+          </svg>
+        </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <span class="small-circle">Olalao Jeanne</span>
+          felis in auctor tempor. Mauris vitae
           sem
           in orci tempus tincidunt. Maecenas sodales facilisis purus ut euismod. Orci varius natoque penatibus et magnis
           dis
-          parturient montes, nascetur ridiculus mus. <span class="highlight">Quisque vehicula</span> 
+          parturient montes, nascetur ridiculus mus. <span class="highlight">Quisque vehicula</span>
           , neque sed fringilla varius, augue neque lacinia
           mi,
           vel consectetur metus elit nec tellus. Sed varius feugiat tempus. Aenean lectus tortor, lacinia sodales
           sagittis
-          et, <span class="underline">porttitor</span> vitae nisi. Phasellus congue mauris ut justo tempus, vel ultricies dui pharetra. Donec dapibus
+          et, <span class="underline">porttitor</span> vitae nisi. Phasellus congue mauris ut justo tempus, vel
+          ultricies dui pharetra. Donec dapibus
           tempor ipsum eu egestas. Donec est tortor, sagittis eget est id, semper fermentum enim. Sed vel nunc molestie,
           porttitor eros ac, dignissim felis.</p>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam distinctio architecto itaque veniam maxime
@@ -20,7 +41,8 @@
           reprehenderit nam minima cum porro!</p>
       </div>
       <div class="description-photo">
-        <svg width="101" height="95" class="strokes-top" viewBox="0 0 101 95" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg width="101" height="95" class="strokes-top" viewBox="0 0 101 95" fill="none"
+          xmlns="http://www.w3.org/2000/svg">
           <path
             d="M77.7089 45.1393C77.7089 49.4089 75.7409 54.5989 74.6688 57.7839C73.2231 62.079 71.9205 66.9442 70.2606 70.7798C68.491 74.8689 66.8117 79.4266 64.8729 83.0731C63.979 84.7543 62.842 88.3074 61.9003 89.3954"
             stroke-width="10" stroke-linecap="round" />
@@ -41,7 +63,8 @@
         <div class="img-container">
           <img src="@/assets/images/marché_.png" alt="photo de Jeanne">
         </div>
-        <svg width="101" height="95" class="strokes-bottom" viewBox="0 0 101 95" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg width="101" height="95" class="strokes-bottom" viewBox="0 0 101 95" fill="none"
+          xmlns="http://www.w3.org/2000/svg">
           <path
             d="M77.7089 45.1393C77.7089 49.4089 75.7409 54.5989 74.6688 57.7839C73.2231 62.079 71.9205 66.9442 70.2606 70.7798C68.491 74.8689 66.8117 79.4266 64.8729 83.0731C63.979 84.7543 62.842 88.3074 61.9003 89.3954"
             stroke="white" stroke-width="10" stroke-linecap="round" />
--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
     <Description />
+    <ContactForm />
   </div>
 </template>
 

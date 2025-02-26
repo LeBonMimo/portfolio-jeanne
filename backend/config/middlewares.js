f39ae2ed8a440c99c797c18f8429439b78325d0e
LeBonMimo <schwab.guillaume2310@gmail.com>
2025-02-26 09:40:27 +0100

    change middleware cors

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,8 +1,29 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'img-src': ["'self'", 'data:', 'blob:', 'https://api.olalao-jeanne.fr'], // Ajoute ton domaine API ici
+          'media-src': ["'self'", 'data:', 'blob:', 'https://api.olalao-jeanne.fr'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['https://olalao-jeanne.fr', 'https://www.olalao-jeanne.fr'], // Ajoute ici le domaine de ton frontend
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',

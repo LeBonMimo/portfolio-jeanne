9480a93be645774919a8f61aa912deb3a8dca8d8
LeBonMimo <schwab.guillaume2310@gmail.com>
2025-02-28 11:38:58 +0100

    correction fichier middlewares.js

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -7,8 +7,9 @@ module.exports = [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'img-src': ["'self'", 'data:', 'blob:', 'https://api.olalao-jeanne.fr'], // Ajoute ton domaine API ici
-          'media-src': ["'self'", 'data:', 'blob:', 'https://api.olalao-jeanne.fr'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://api.olalao-jeanne.fr', 'https://olalao-jeanne.fr', '*'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://api.olalao-jeanne.fr', 'https://olalao-jeanne.fr', '*'],
+          'connect-src': ["'self'", 'https://api.olalao-jeanne.fr', 'https://olalao-jeanne.fr', '*'],
           upgradeInsecureRequests: null,
         },
       },
@@ -18,10 +19,11 @@ module.exports = [
     name: 'strapi::cors',
     config: {
       enabled: true,
-      origin: ['https://olalao-jeanne.fr', 'https://www.olalao-jeanne.fr'], // Ajoute ici le domaine de ton frontend
+      origin: ['https://olalao-jeanne.fr', 'https://www.olalao-jeanne.fr', 'https://api.olalao-jeanne.fr'],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       keepHeaderOnError: true,
+      credentials: true,
     },
   },
   'strapi::poweredBy',

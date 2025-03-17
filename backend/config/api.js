eb894a9531907cd48d291832a7c9af49616a5e23
LeBonMimo <schwab.guillaume2310@gmail.com>
2025-03-17 12:54:08 +0100

    revert previous fix

--- backend/config/api.js
+++ backend/config/api.js
@@ -3,12 +3,5 @@ module.exports = {
     defaultLimit: 25,
     maxLimit: 100,
     withCount: true,
-    cors: { 
-      enabled: true, 
-      origin: process.env.CORS_ORIGIN?.split(',') || [
-        'https://olalao-jeanne.fr',
-        'https://www.olalao-jeanne.fr',
-      ], 
-    },
   },
 };
